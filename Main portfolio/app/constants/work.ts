6bb53f467790c4dbc869b20e0a77685776420502
ranjitepili08-creator <ranjitepili08@gmail.com>
2026-08-31 11:12:19 +0530

    v4

--- Main portfolio/app/constants/work.ts
+++ Main portfolio/app/constants/work.ts
@@ -38,7 +38,7 @@ export const WORK_TIMELINE: WorkTimelinePoint[] = [
     position: 'left',
   },
   {
-    point: new THREE.Vector3(1, 1, 12),
+    point: new THREE.Vector3(2, 2, -11),
     year: new Date().toLocaleDateString('default', { year: 'numeric' }),
     title: 'Shah and Anchor Kutchhi Enginnering college...',
     subtitle: 'BTECH - ECS',
